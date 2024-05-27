90e21553f2df319f3e7448e558849241718313f8
codewithabhi001 <abhisheksingh9709844475@gmail.com>
2024-05-28 00:53:29 +0530

    button fix

--- .history/client/src/App_20240528005216.jsx
+++ .history/client/src/App_20240528005216.jsx
@@ -48,7 +48,7 @@ const App = () => {
             left: 0,
             width: "100%",
             height: "100%",
-            backgroundColor: "rgba(0, 0, 0, 0.2)",
+            backgroundColor: "rgba(0, 0, 0, 0.5)",
           }}
         ></div>
         <Navbar />
